cbdc1cc04fbabcc0094eea0e4d70d20d0594f2f3
Juliabinotti <00001110541971sp@al.educacao.sp.gov.br>
2024-11-25 18:47:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Inglês',
+    'O que significa tea?',
+    'significa Chá'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a Maior cidade do Brasil?',
+    'São Paulo'
+)
+
+criaCartao(
+    'Fisica ',
+    'O que é inercia?',
+    'A inércia é uma propriedade geral da matéria. Considere um corpo não submetido à ação de forças ou submetido a um conjunto de forças de resultante nula'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual o maior Planeta do sistema solar?',
+    'Júpiter'
+)
